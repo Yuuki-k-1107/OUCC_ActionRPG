f5e56068efaa7ee502bf88db4c19d1bee0a16d2e
Yuuki-k-1107 <yukun1107k@softbank.ne.jp>
2020-06-09 23:54:59 +0900

    ARPG
    test1

---
+++ Bad action game 1/Assets/Scenes/Plyrctlr.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Plyrctlr : MonoBehaviour
+{
+    public Rigidbody2D Rig2D;
+    GameObject director;
+    float liftForce = 500.0f;
+    float moveForce = 25.0f;
+    float limitspeed = 5.0f;
+    public static Vector3 respawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Rig2D = GetComponent<Rigidbody2D>();
+        //this.director = GameObject.Find("kantoku");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       /* if (other.gameObject.tag == "credit")
+        {
+            this.director.GetComponent<UI>().GetCredit();
+            Destroy(other.gameObject);
+        }*/
+    }
+
+    void OnTriggerStay2D(Collider2D other2)
+    {
+        /*if (other2.gameObject.tag == "door")
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                stscore = UI.scored;
+                stlife = UI.lived;
+                respawn = new Vector3(-6.5f, 3.0f, 0);
+                SceneManager.LoadScene("scene2");
+            }
+        }
+        else if (other2.gameObject.tag == "door2")
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                respawn = new Vector3(100.5f, 3.0f, 0);
+                transform.position = respawn;
+            }
+        }*/
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl)) limitspeed = 10.0f;
+        else limitspeed = 5.0f;
+        float speed = Mathf.Abs(this.Rig2D.velocity.x);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Mathf.Abs(this.Rig2D.velocity.y) <= 0.02f)
+        {
+            this.Rig2D.AddForce(transform.up * liftForce);
+        }
+        if (speed < limitspeed)
+        {
+            this.Rig2D.AddForce(transform.right * Input.GetAxis("Horizontal") * this.moveForce);
+        }
+        if (this.Rig2D.transform.position.y < -5.0f)
+        {
+            //this.director.GetComponent<UI>().LossLife();
+            this.Rig2D.velocity = new Vector2(0, 0);
+            transform.position = respawn;
+        }
+    }
+}
---
+++ Bad action game 1/Assets/Scenes/cameractlr.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameractlr : MonoBehaviour
+{
+    GameObject you;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.you = GameObject.Find("arpg-player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 yourposition = this.you.transform.position;
+        if (yourposition.x >= 0 && yourposition.y >= 0)
+        {
+            transform.position = new Vector3(yourposition.x, yourposition.y, transform.position.z);
+        }
+        else if (yourposition.x >= 0)
+        {
+            transform.position = new Vector3(yourposition.x, 0, transform.position.z);
+        }
+        else if (yourposition.y >= 0)
+        {
+            transform.position = new Vector3(0, yourposition.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(0, 0, transform.position.z);
+        }
+    }
+}

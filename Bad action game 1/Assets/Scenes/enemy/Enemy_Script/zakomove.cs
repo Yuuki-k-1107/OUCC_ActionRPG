c39016c208b15acb8b534b36dc7b7e431de55723
takuemon <66411888+takuemon@users.noreply.github.com>
2020-06-14 22:35:48 +0900

    anime

---
+++ Bad action game 1/Assets/Scenes/enemy/Enemy_Script/zakomove.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zakomove : MonoBehaviour {
+    #region//インスペクター
+    [Header("移動速度")] public float speed;
+    [Header("重力")] public float gravity;
+    [Header("画面外でも行動する")] public bool nonVisibleAct;
+    #endregion
+
+    #region//private変数
+    private Rigidbody2D rb = null;
+    private SpriteRenderer sr = null;
+    private Animator anim = null;
+    private ObjectCollision oc = null;
+    private BoxCollider2D col = null;
+    private bool rightTleftF = false;
+    private bool isDead = false;
+    private float deadTimer = 0.0f;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        oc = GetComponent<ObjectCollision>();
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!oc.playerStepOn)
+        {
+            if (sr.isVisible || nonVisibleAct)
+            {
+                int xVector = -1;
+                if (rightTleftF)
+                {
+                    xVector = 1;
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                rb.velocity = new Vector2(xVector * speed, -gravity);
+                anim.SetBool("walk", true);
+            }
+            else
+            {
+                anim.SetBool("walk", false);
+            }
+        }
+        else
+        {
+            if (!isDead)
+            {
+                anim.Play("dead");
+                rb.velocity = new Vector2(0, -gravity);
+                isDead = true;
+                col.enabled = false;
+            }
+            else
+            {
+                transform.Rotate(new Vector3(0, 0, 5));
+                if (deadTimer > 3.0f)
+                {
+                    Destroy(this.gameObject);
+                }
+            
+        
+            else
+            {
+                deadTimer += Time.deltaTime;
+            }
+           }
+    }
+}
+}

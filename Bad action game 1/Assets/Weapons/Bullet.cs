2837bbbbba1e3527e61e9b9f3f23edcb5d0e3ff1
Mizu-Shiro <66170570+Mizu-Shiro@users.noreply.github.com>
2020-06-15 20:31:21 +0900

    先輩に言われたのでやった。理解はしていない。

---
+++ Bad action game 1/Assets/Weapons/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("弾速")] public float bulspd = 3.0f;//bullet speed
+    [Header("射程")] public float bulrng = 100.0f;//bullet range
+    private Rigidbody2D rb;
+    private Vector3 dfpos;//default position
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if(rb == null)
+        {
+            Debug.Log("設定どっかで抜けてる");
+            Destroy(this.gameObject);
+        }
+        dfpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float d = Vector3.Distance(transform.position, dfpos);
+        //射程に達する
+        if (d > bulrng)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            rb.MovePosition(transform.position += Vector3.right * Time.deltaTime * bulspd);
+        }
+    }
+    private void OntriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Bad action game 1/Assets/Weapons/OnlyShotPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyShotPlayer : MonoBehaviour
+{
+    public string shotbutton;
+    private bool gunbut = false;
+    public bool Gunbut()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gunbut = true;
+        }
+        else
+        {
+            gunbut = false;
+        }
+        return gunbut;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

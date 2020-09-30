e14ff6b5ebed3a7f2dc33c2d29df45083aafe1cb
Shiokai <66198929+Shiokai@users.noreply.github.com>
2020-09-30 23:46:53 +0900

    Unite scripts of spear and bat
    槍とバットのスクリプト統合
    武器変更作成途中

---
+++ Bad action game 1/Assets/Asset/ユニティちゃんドット絵素材（Co89）/CloseAttack/CloseAttackManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseAttackManager : MonoBehaviour
+{
+    private Animator anim;
+
+    [SerializeField]
+    private Collider2D spearCollider;
+
+    [SerializeField]
+    private Collider2D batCollider;
+
+    public int CloseWeponIndex = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "bat")
+        {
+            CloseWeponIndex = 1;
+        }
+        else if (other.gameObject.tag == "spear")
+        {
+            CloseWeponIndex = 2;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+        if (Input.GetButtonDown("CloseAttack"))
+        {
+                if (CloseWeponIndex == 1)
+                {
+                    anim.SetBool("Spear", true);
+                }
+                else if (CloseWeponIndex == 2)
+                {
+                    anim.SetBool("Bat", true);
+                }
+
+        }
+        else if (Input.GetButtonUp("CloseAttack"))
+        {
+            anim.SetBool("Spear", false);
+
+            anim.SetBool("Bat", false);
+        }
+    }
+
+}
---
+++ Bad action game 1/Assets/Asset/ユニティちゃんドット絵素材（Co89）/CloseAttack/unitychan_spear/ControlCollider_spear.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ControlCollider_spear : MonoBehaviour
+{
+    public Collider2D spearCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject spearcollider = transform.Find("spear_collider").gameObject;
+
+        spearCollider = spearcollider.GetComponent<BoxCollider2D>();
+
+        spearCollider.enabled = false;
+    }
+    public void on_spear()
+    {
+        spearCollider.enabled = true;
+
+    }
+
+    public void off_spear()
+    {
+        spearCollider.enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
